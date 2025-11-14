cf955006008b0df51ee99d8b08c44376042b8c3f
koblikov <koblikov.i.a@gmail.com>
2025-11-14 13:44:26 +0300

    выделены вспомогательные методы и классы для групп
    Signed-off-by: koblikov <koblikov.i.a@gmail.com>

--- test_add_grup.py
+++ test_add_grup.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+import group
+from group import Group
+
 
 class Testaddgrup(unittest.TestCase):
+
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
@@ -15,30 +17,67 @@ class Testaddgrup(unittest.TestCase):
     
     def test_addgrup(self):
         wd = self.wd
+        # открываем домашнюю страницу
+        self.open_home_page(wd)
+        #Логинимся
+        self.login(wd,"admin","secret")
+        #переходим на вкладку группы
+        self.open_groups_page(wd)
+        #Создаем новую группу
+        self.create_group(wd,Group( "grup1", "grup1", "grup1"))
+        # переходим на вкладку группы
+        self.open_groups_page(wd)
+        #выходим из уз
+        wd.find_element("link text", "Logout").click()
+
+    def test_add_empt_grup(self):
+        wd = self.wd
+        # открываем домашнюю страницу
+        self.open_home_page(wd)
+        #Логинимся
+        self.login(wd,"admin","secret")
+        #переходим на вкладку группы
+        self.open_groups_page(wd)
+        #Создаем новую группу
+        self.create_group(wd, Group("", "", ""))
+        # переходим на вкладку группы
+        self.open_groups_page(wd)
+        #выходим из уз
+        wd.find_element("link text", "Logout").click()
+
+    def create_group(self, wd, group):
+        # Нажимаем создать новую группу
+        wd.find_element(By.NAME, "new").click()
+        # Вводим название группы
+        self.send_key_by_name(wd, "group_name", group.name)
+        # вводим хэадер группы
+        self.send_key_by_name(wd, "group_header", group.header)
+        # вводим футер группы
+        self.send_key_by_name(wd, "group_footer", group.footer)
+        # подтверждаем создание группы
+        wd.find_element("name", "submit").click()
+
+    def open_groups_page(self, wd):
+        wd.find_element("link text", "groups").click()
+
+    def login(self, wd,name,passwd):
+        # Вводим логин
+        self.send_key_by_name(wd, "user", name)
+        # Вводим пароль
+        self.send_key_by_name(wd, "pass", passwd)
+        # Нажимаем кнопку войти(залогиниться)
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+
         wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("grup1")
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("grup1")
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("grup1")
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-    
+
+    def send_key_by_name(self,wd,name,text):
+        wd = self.wd
+        wd.find_element(By.NAME, name).click()
+        wd.find_element(By.NAME, name).clear()
+        wd.find_element(By.NAME, name).send_keys(text)
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
